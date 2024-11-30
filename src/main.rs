5c8b232a2c621bdcaf844b2a3341824fba5826c1
Brian Tum <brian.tum10@yahoo.com>
2024-11-30 18:54:31 +0300

    Add insert seed data into electorate table

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ mod database;
 
 fn main() {
     // database::create_database::create_db();
-    database::electorate_table::create_electorate_table();
+    database::electorate_table::create_electorate_table()
+    .expect("Couldnt create or insert data to electorate");
 }
