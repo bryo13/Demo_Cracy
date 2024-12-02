eb5befc45d7175e490992c3243d2db7e5e7a0c1a
Brian Tum <brian.tum10@yahoo.com>
2024-12-02 17:52:14 +0300

    voting module

---
+++ src/voting/voting_table.rs
@@ -0,0 +1,13 @@
+/// create voting table with the columns holding
+/// the candidates and voters_ID
+use sqlx::SqlitePool;
+
+#[tokio::main]
+async fn get_candidates_ID() -> Result<String, String>{
+    let gc_pool = 
+}
+
+#[tokio::main]
+async fn create_voting_table() -> Result<String, String> {
+
+}
