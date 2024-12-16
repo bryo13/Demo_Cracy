192842c7c0f258e312178bdb7de88de92774a3b3
Brian Tum <brian.tum10@yahoo.com>
2024-12-16 22:08:01 +0300

    confirm user is regestered to vote

--- voting/src/voting_day.rs
+++ voting/src/voting_day.rs
@@ -1,10 +1,41 @@
-/// Computation that handles results of a voting day and logic
-/// involved i.e age verification
+/// On voting day:
+///  -> The electorate will be asked to enter their ID number
+///     the ID will be verified if he/she is present in the DB
+///     If he/she is above 18.
+///  -> Once this parameters are met, they will be asked to choose
+///     their prefered candidate, this updates the votes table.
+///     Then their less prefered candidate until all candidates are captured.
+///  -> For the prefered candidate, they recieve a total of the number of candidates,
+///     i.e if there are three candidates, an electorate's fav candidate will recieve
+///     3 points, the 2nd prefered candidate 2 and the least prefered candidate 1.
+
+use data::create_database::DB_PATH;
+use sqlx::{sqlite::SqliteRow, Row, SqlitePool};
+use std::io;
+use chrono::NaiveDate;
 
 const VOTING_DATE: &str = "2024-11-05";
 
 #[tokio::main]
-async fn electorate_details() -> Result<SqliteRow, sqlx::Error> {}
+async fn electorate_details() -> Result<SqliteRow, sqlx::Error> {
+    let voting_pool = SqlitePool::connect(DB_PATH).await?;
+
+    let mut id_number: String = String::new();
+    io::stdin()
+        .read_line(&mut id_number)
+        .expect("couldnt read user ID");
+
+    let query_user = sqlx::query(
+        "SELECT * FROM electorate_table 
+        WHERE ID_number = ?;",
+    )
+    .bind(id_number.trim())
+    .fetch_one(&voting_pool)
+    .await?;
+
+    Ok(query_user)
+
+}
 
 fn valid_age(details: Result<SqliteRow, sqlx::Error>) -> bool {
     match details {
